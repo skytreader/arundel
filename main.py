b6d4d392a58b2d945e5ae04606fe9d53389f7dc5
Chad Estioco <chadestioco@gmail.com>
2021-07-04 20:05:12 +0800

    Add bottle.

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+from bottle import route, run
+
+@route("/")
+def root():
+    return "Welcome to Arundel"
+
 if __name__ == "__main__":
-    print(input("Enter query: "))
+    run(host="0.0.0.0", port=8000)
